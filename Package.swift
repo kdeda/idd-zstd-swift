2c3da92c9953fa17a6d578691453c262f694c378
Klajd Deda <kdeda@id-design.com>
2023-03-09 21:52:13 -0500

    Branching off to Version 2.x.x, Pure Swift, no more Obj-C. Version 1.x is placed in hospice.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "idd-zstd-swift",
     platforms: [
-        // .iOS(.v13),
-        .macOS(.v10_13)
+        .iOS(.v15),
+        .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -15,8 +15,7 @@ let package = Package(
             name: "ZSTDSwift",
             targets: ["ZSTDLib", "ZSTDSwift"]),
     ],
-    dependencies: [
-    ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
